e583e59fbbd7263d9a367ea73413eeec9e2ecacc
Sujay Garlanka <sujay.garlanka@gmail.com>
2020-11-13 22:29:16 -0500

    update ui

--- ios/TennisBot/AutonomousViewController.swift
+++ ios/TennisBot/AutonomousViewController.swift
@@ -10,10 +10,316 @@ import UIKit
 import CoreML
 import Vision
 import ImageIO
+import AVKit
 
-class AutonomousViewController: UIViewController, UINavigationControllerDelegate {
+class AutonomousViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
 
-       @IBOutlet weak var photoImageView: UIImageView?
+    // Main view for showing camera content.
+    @IBOutlet weak var previewView: UIView!
+    // AVCapture variables to hold sequence data
+    var session: AVCaptureSession?
+    var previewLayer: AVCaptureVideoPreviewLayer?
+    
+    var videoDataOutput: AVCaptureVideoDataOutput?
+    var videoDataOutputQueue: DispatchQueue?
+    
+    var captureDevice: AVCaptureDevice?
+    var captureDeviceResolution: CGSize = CGSize()
+    
+    // Vision requests
+    private var detectionRequests: [VNCoreMLRequest]?
+    private var trackingRequests: [VNTrackObjectRequest]?
+    
+    lazy var sequenceRequestHandler = VNSequenceRequestHandler()
+    
+    // MARK: UIViewController overrides
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.session = self.setupAVCaptureSession()
+        
+        self.prepareVisionRequest()
+        
+        self.session?.startRunning()
+    }
+    
+    /// - Tag: CreateCaptureSession
+    fileprivate func setupAVCaptureSession() -> AVCaptureSession? {
+        let captureSession = AVCaptureSession()
+        do {
+            let inputDevice = try self.configureBackCamera(for: captureSession)
+            self.configureVideoDataOutput(for: inputDevice.device, resolution: inputDevice.resolution, captureSession: captureSession)
+            self.designatePreviewLayer(for: captureSession)
+            return captureSession
+        } catch let executionError as NSError {
+            self.presentError(executionError)
+        } catch {
+            self.presentErrorAlert(message: "An unexpected failure has occured")
+        }
+        
+        self.teardownAVCapture()
+        
+        return nil
+    }
+    
+    fileprivate func configureBackCamera(for captureSession: AVCaptureSession) throws -> (device: AVCaptureDevice, resolution: CGSize) {
+        let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: .video, position: .back)
+        
+        if let device = deviceDiscoverySession.devices.first {
+            if let deviceInput = try? AVCaptureDeviceInput(device: device) {
+                if captureSession.canAddInput(deviceInput) {
+                    captureSession.addInput(deviceInput)
+                }
+                
+                if let highestResolution = self.highestResolution420Format(for: device) {
+                    try device.lockForConfiguration()
+                    device.activeFormat = highestResolution.format
+                    device.unlockForConfiguration()
+                    
+                    return (device, highestResolution.resolution)
+                }
+            }
+        }
+        
+        throw NSError(domain: "ViewController", code: 1, userInfo: nil)
+    }
+    
+    /// - Tag: ConfigureDeviceResolution
+    fileprivate func highestResolution420Format(for device: AVCaptureDevice) -> (format: AVCaptureDevice.Format, resolution: CGSize)? {
+        var highestResolutionFormat: AVCaptureDevice.Format? = nil
+        var highestResolutionDimensions = CMVideoDimensions(width: 0, height: 0)
+        
+        for format in device.formats {
+            let deviceFormat = format as AVCaptureDevice.Format
+            
+            let deviceFormatDescription = deviceFormat.formatDescription
+            if CMFormatDescriptionGetMediaSubType(deviceFormatDescription) == kCVPixelFormatType_420YpCbCr8BiPlanarFullRange {
+                let candidateDimensions = CMVideoFormatDescriptionGetDimensions(deviceFormatDescription)
+                if (highestResolutionFormat == nil) || (candidateDimensions.width > highestResolutionDimensions.width) {
+                    highestResolutionFormat = deviceFormat
+                    highestResolutionDimensions = candidateDimensions
+                }
+            }
+        }
+        
+        if highestResolutionFormat != nil {
+            let resolution = CGSize(width: CGFloat(highestResolutionDimensions.width), height: CGFloat(highestResolutionDimensions.height))
+            return (highestResolutionFormat!, resolution)
+        }
+        
+        return nil
+    }
+    
+    /// - Tag: CreateSerialDispatchQueue
+    fileprivate func configureVideoDataOutput(for inputDevice: AVCaptureDevice, resolution: CGSize, captureSession: AVCaptureSession) {
+        
+        let videoDataOutput = AVCaptureVideoDataOutput()
+        videoDataOutput.alwaysDiscardsLateVideoFrames = true
+        
+        // Create a serial dispatch queue used for the sample buffer delegate as well as when a still image is captured.
+        // A serial dispatch queue must be used to guarantee that video frames will be delivered in order.
+        let videoDataOutputQueue = DispatchQueue(label: "com.example.apple-samplecode.VisionFaceTrack")
+        videoDataOutput.setSampleBufferDelegate(self, queue: videoDataOutputQueue)
+        
+        if captureSession.canAddOutput(videoDataOutput) {
+            captureSession.addOutput(videoDataOutput)
+        }
+        
+        videoDataOutput.connection(with: .video)?.isEnabled = true
+        
+        if let captureConnection = videoDataOutput.connection(with: AVMediaType.video) {
+            if captureConnection.isCameraIntrinsicMatrixDeliverySupported {
+                captureConnection.isCameraIntrinsicMatrixDeliveryEnabled = true
+            }
+        }
+        
+        self.videoDataOutput = videoDataOutput
+        self.videoDataOutputQueue = videoDataOutputQueue
+        
+        self.captureDevice = inputDevice
+        self.captureDeviceResolution = resolution
+    }
+    
+    /// - Tag: DesignatePreviewLayer
+    fileprivate func designatePreviewLayer(for captureSession: AVCaptureSession) {
+        let videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        self.previewLayer = videoPreviewLayer
+        
+        videoPreviewLayer.name = "CameraPreview"
+        videoPreviewLayer.backgroundColor = UIColor.black.cgColor
+        videoPreviewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        
+        if let previewRootLayer = self.previewView?.layer {
+//            self.rootLayer = previewRootLayer
+            
+            previewRootLayer.masksToBounds = true
+            videoPreviewLayer.frame = previewRootLayer.bounds
+            previewRootLayer.addSublayer(videoPreviewLayer)
+        }
+    }
+    
+    // Removes infrastructure for AVCapture as part of cleanup.
+    fileprivate func teardownAVCapture() {
+        self.videoDataOutput = nil
+        self.videoDataOutputQueue = nil
+        
+        if let previewLayer = self.previewLayer {
+            previewLayer.removeFromSuperlayer()
+            self.previewLayer = nil
+        }
+    }
+    
+    // MARK: Performing Vision Requests
+    
+    /// - Tag: WriteCompletionHandler
+    fileprivate func prepareVisionRequest() {
+        
+        //self.trackingRequests = []
+        var requests = [VNTrackObjectRequest]()
+        
+        let model = try! VNCoreMLModel(for: YOLOv3Int8LUT().model)
+        
+        let personRequest = VNCoreMLRequest(model: model, completionHandler: { [weak self] request, error in
+            DispatchQueue.main.async {
+                guard let results = request.results else {
+                    print("Unable to detect anything.\n\(error!.localizedDescription)")
+                    return
+                }
+            
+                let detections = results as! [VNRecognizedObjectObservation]
+                for detection in detections {
+                   // The coordinates are normalized to the dimensions of the processed image, with the origin at the image's lower-left corner.
+                    if (detection.labels[0].identifier == "person") {
+                        let personTrackingRequest = VNTrackObjectRequest(detectedObjectObservation: detection)
+                        requests.append(personTrackingRequest)
+                    }
+                }
+                self?.trackingRequests = requests
+            }
+        })
+        
+        personRequest.imageCropAndScaleOption = .scaleFit
+        
+        // Start with detection.  Find person, then track it.
+        self.detectionRequests = [personRequest]
+        
+        self.sequenceRequestHandler = VNSequenceRequestHandler()
+    }
+    
+    // MARK: AVCaptureVideoDataOutputSampleBufferDelegate
+    /// - Tag: PerformRequests
+    // Handle delegate method callback on receiving a sample buffer.
+    public func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        
+        var requestHandlerOptions: [VNImageOption: AnyObject] = [:]
+        
+        let cameraIntrinsicData = CMGetAttachment(sampleBuffer, key: kCMSampleBufferAttachmentKey_CameraIntrinsicMatrix, attachmentModeOut: nil)
+        if cameraIntrinsicData != nil {
+            requestHandlerOptions[VNImageOption.cameraIntrinsics] = cameraIntrinsicData
+        }
+        
+        guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
+            print("Failed to obtain a CVPixelBuffer for the current output frame.")
+            return
+        }
+        
+        let exifOrientation = self.exifOrientationForCurrentDeviceOrientation()
+        
+        guard let requests = self.trackingRequests, !requests.isEmpty else {
+            // No tracking object detected, so perform initial detection
+            let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer,
+                                                            orientation: exifOrientation,
+                                                            options: requestHandlerOptions)
+            
+            do {
+                guard let detectRequests = self.detectionRequests else {
+                    return
+                }
+                try imageRequestHandler.perform(detectRequests)
+            } catch let error as NSError {
+                NSLog("Failed to perform FaceRectangleRequest: %@", error)
+            }
+            return
+        }
+        
+        do {
+            try self.sequenceRequestHandler.perform(requests,
+                                                     on: pixelBuffer,
+                                                     orientation: exifOrientation)
+        } catch let error as NSError {
+            NSLog("Failed to perform SequenceRequest: %@", error)
+        }
+        
+        // Setup the next round of tracking.
+        var newTrackingRequests = [VNTrackObjectRequest]()
+        for trackingRequest in requests {
+            
+            guard let results = trackingRequest.results else {
+                return
+            }
+            
+            guard let observation = results[0] as? VNDetectedObjectObservation else {
+                return
+            }
+            
+            if !trackingRequest.isLastFrame {
+                if observation.confidence > 0.3 {
+                    trackingRequest.inputObservation = observation
+                } else {
+                    trackingRequest.isLastFrame = true
+                }
+                newTrackingRequests.append(trackingRequest)
+            }
+        }
+        self.trackingRequests = newTrackingRequests
+        
+        if newTrackingRequests.isEmpty {
+            // Nothing to track, so abort.
+            return
+        }
+    }
+    
+    // MARK: Helper Methods for Handling Device Orientation & EXIF
+    
+    fileprivate func radiansForDegrees(_ degrees: CGFloat) -> CGFloat {
+        return CGFloat(Double(degrees) * Double.pi / 180.0)
+    }
+    
+    func exifOrientationForDeviceOrientation(_ deviceOrientation: UIDeviceOrientation) -> CGImagePropertyOrientation {
+        
+        switch deviceOrientation {
+        case .portraitUpsideDown:
+            return .rightMirrored
+            
+        case .landscapeLeft:
+            return .downMirrored
+            
+        case .landscapeRight:
+            return .upMirrored
+            
+        default:
+            return .leftMirrored
+        }
+    }
+    
+    func exifOrientationForCurrentDeviceOrientation() -> CGImagePropertyOrientation {
+        return exifOrientationForDeviceOrientation(UIDevice.current.orientation)
+    }
+    
+    // MARK: Helper Methods for Error Presentation
+    
+    fileprivate func presentErrorAlert(withTitle title: String = "Unexpected Failure", message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        self.present(alertController, animated: true)
+    }
+    
+    fileprivate func presentError(_ error: NSError) {
+        self.presentErrorAlert(withTitle: "Failed with error \(error.code)", message: error.localizedDescription)
+    }
+    
+    
+    @IBOutlet weak var photoImageView: UIImageView?
        
        lazy var detectionRequest: VNCoreMLRequest = {
            do {
@@ -29,12 +335,6 @@ class AutonomousViewController: UIViewController, UINavigationControllerDelegate
            }
        }()
        
-       @IBAction func testPhoto(sender: UIButton) {
-           let vc = UIImagePickerController()
-           vc.sourceType = .photoLibrary
-           vc.delegate = self
-           present(vc, animated: true)
-       }
        
        private func updateDetections(for image: UIImage) {
 
