acffc28e20a9dff22df74f0c320a5dce63ee0193
Sujay Garlanka <sujay.garlanka@gmail.com>
2021-04-10 18:12:49 -0400

    Update control.py

--- raspberry_pi/control.py
+++ raspberry_pi/control.py
@@ -19,63 +19,63 @@ GPIO.setup(BACK_POWER, GPIO.OUT)
 
 
 def left(power):
-    GPIO.output(FRONT_POWER, GPIO.HIGH)
-    GPIO.output(FRONT_1, GPIO.LOW)
-    GPIO.output(FRONT_2, GPIO.HIGH)
-    
+	GPIO.output(FRONT_POWER, GPIO.HIGH)
+	GPIO.output(FRONT_1, GPIO.LOW)
+	GPIO.output(FRONT_2, GPIO.HIGH)
+
 def right(power):
-    GPIO.output(FRONT_POWER, GPIO.HIGH)
-    GPIO.output(FRONT_1, GPIO.HIGH)
-    GPIO.output(FRONT_2, GPIO.LOW)
-    
+	GPIO.output(FRONT_POWER, GPIO.HIGH)
+	GPIO.output(FRONT_1, GPIO.HIGH)
+	GPIO.output(FRONT_2, GPIO.LOW)
+
 def center():
-    GPIO.output(FRONT_POWER, GPIO.LOW)
-    GPIO.output(FRONT_1, GPIO.LOW)
-    GPIO.output(FRONT_2, GPIO.LOW)
+	GPIO.output(FRONT_POWER, GPIO.LOW)
+	GPIO.output(FRONT_1, GPIO.LOW)
+	GPIO.output(FRONT_2, GPIO.LOW)
 
 def forward(power):
-    GPIO.output(BACK_POWER, GPIO.HIGH)
-    GPIO.output(BACK_1, GPIO.HIGH)
-    GPIO.output(BACK_2, GPIO.LOW)
-    
+	GPIO.output(BACK_POWER, GPIO.HIGH)
+	GPIO.output(BACK_1, GPIO.HIGH)
+	GPIO.output(BACK_2, GPIO.LOW)
+
 def backward(power):
-    GPIO.output(BACK_POWER, GPIO.HIGH)
-    GPIO.output(BACK_1, GPIO.LOW)
-    GPIO.output(BACK_2, GPIO.HIGH)
-    
+	GPIO.output(BACK_POWER, GPIO.HIGH)
+	GPIO.output(BACK_1, GPIO.LOW)
+	GPIO.output(BACK_2, GPIO.HIGH)
+
 def stop():
-    GPIO.output(BACK_POWER, GPIO.LOW)
-    GPIO.output(BACK_1, GPIO.LOW)
-    GPIO.output(BACK_2, GPIO.LOW)
-    
+	GPIO.output(BACK_POWER, GPIO.LOW)
+	GPIO.output(BACK_1, GPIO.LOW)
+	GPIO.output(BACK_2, GPIO.LOW)
+
 class WebSocket(tornado.websocket.WebSocketHandler):
 
-    def on_message(self, message):
-        """Evaluates the function pointed to by json-rpc."""
-	if message == 'f':
-		forward(0)
-		self.write_message("forward")
-	elif message == 'b':
-		backward(0)
-		self.write_message("backward")
-	elif message == 's':
-		stop()
-		self.write_message("stop")
-	elif message == 'r':
-		right(0)
-		self.write_message("right")
-	elif message == 'l':
-		left(0)
-		self.write_message("left")
-	elif message == 'c':
-		center()
-		self.write_message('center')
-	else:
-		self.write_message('command not recognized')
-    
+	def on_message(self, message):
+	"""Evaluates the function pointed to by json-rpc."""
+		if message == 'f':
+			forward(0)
+			self.write_message("forward")
+		elif message == 'b':
+			backward(0)
+			self.write_message("backward")
+		elif message == 's':
+			stop()
+			self.write_message("stop")
+		elif message == 'r':
+			right(0)
+			self.write_message("right")
+		elif message == 'l':
+			left(0)
+			self.write_message("left")
+		elif message == 'c':
+			center()
+			self.write_message('center')
+		else:
+			self.write_message('command not recognized')
+
 if __name__ == "__main__":
-    handlers = [(r"/websocket", WebSocket)]
-    application = tornado.web.Application(handlers)
-    application.listen(8000)
-    tornado.ioloop.IOLoop.instance().start()
+	handlers = [(r"/websocket", WebSocket)]
+	application = tornado.web.Application(handlers)
+	application.listen(8000)
+	tornado.ioloop.IOLoop.instance().start()
 
